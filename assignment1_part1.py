041c461a2cd7ea1ea513bbc67d58dbd8dafce9bd
kbourne87 <kpbourne87@gmail.com>
2023-09-04 20:41:16 -0400

    Add files via upload

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,19 +1,25 @@
+def list_divide(numbers, divide=2):
+    clean_count = 0
+    for number in numbers:
+        if number % divide == 0:
+            clean_count += 1
+    return clean_count
 
-def list_divide(numbers, divide):
-    """
-    The function returns the number of elements in the numbers list that are divisibleby divide
-    """
+
+class ListDivideException(Exception):
     pass
 
-def test_list_divide():
-    """
-    Test listDivide
-    """
-    assert listDivide([1,2,3,4,5]) == 2
-    assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
-    assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
-    
-if __name__ == "__main__":
-    testListDivide()
+
+def test_list_divide(numbers, divide=2):
+    try:
+        list_divide(numbers, divide)
+    except:
+        raise ListDivideException
+
+
+test_list_divide([1,2,3,4,5])
+test_list_divide([])
+test_list_divide([2,4,6,8,10])
+test_list_divide([30,54,63,98,100], divide=10)
+test_list_divide([1,2,3,4,5], divide=1)
+test_list_divide([7, "a"])
